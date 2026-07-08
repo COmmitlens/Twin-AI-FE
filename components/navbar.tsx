2c03d1cd25291a1607d018f5d5c0286f367d2c9a
siddharth <siddharthchandrakar007@gmail.com>
2026-07-08 17:52:53 +0530

    fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-//hiiia
+
 
 export function   Navbar() {
   const router = useRouter();
